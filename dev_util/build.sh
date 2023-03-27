7c28e6e87fd71ca48bd422b1786d7d8d8c65dbe4
jacobbox <71558382+jacobbox@users.noreply.github.com>
2023-03-27 06:34:20 +0100

    build will only print success if  successful

--- dev_util/build.sh
+++ dev_util/build.sh
@@ -8,7 +8,5 @@ touch mchy/built/temp.temp
 rm -r mchy/built/*
 
 echo "> build antlr"
-java org.antlr.v4.Tool -Dlanguage=Python3 -no-listener -visitor -o mchy/built grammar/Mchy.g4
-
-echo "> antlr built successfully!"
+java org.antlr.v4.Tool -Dlanguage=Python3 -no-listener -visitor -o mchy/built grammar/Mchy.g4 && echo "> antlr built successfully!"
 
