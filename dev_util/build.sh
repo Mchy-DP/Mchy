073e25464004912eb8d9db78d0f2d3fb673771a7
Mchy-DP <129029093+Mchy-DP@users.noreply.github.com>
2023-03-27 08:13:55 +0100

    CICD Initial work
    * cicd initial attempt
    
    * Added exec permission to .sh util files
    
    * print pwd in testing to aid debug
    
    * Fixed windows cmd usage in ANTLR build
    
    * build will only print success if  successful
    
    * make built directory if missing
    
    * Install antlr too
    
    * Changed setup java
    
    * fixed version mismatch
    
    * fixed further version mismatch
    
    * added debug ls to antlr build
    
    * further debug to antlr build step
    
    * added quotes
    
    * build changes
    
    * moved further debug to failed build case
    
    * fixed borked path
    
    * added temp echo
    
    * Attempted fix for linix build failure
    
    * Added fix to build
    
    * win built fix
    
    * rouge import fixed
    
    * Split github actions
    
    ---------
    
    Co-authored-by: jacobbox <71558382+jacobbox@users.noreply.github.com>

--- dev_util/build.sh
+++ dev_util/build.sh
@@ -4,11 +4,28 @@
 cd "$(dirname "$0")/.."
 
 echo "> clean"
+mkdir -p mchy/built
 touch mchy/built/temp.temp
 rm -r mchy/built/*
 
 echo "> build antlr"
-cmd "/c antlr4 -Dlanguage=Python3 -no-listener -visitor -o mchy/built grammar/Mchy.g4 "
+java -Xmx500M -cp "/usr/local/lib/antlr-4.10-complete.jar:$CLASSPATH" org.antlr.v4.Tool -Dlanguage=Python3 -no-listener -visitor -o "mchy/built" "./grammar/Mchy.g4"
 
-echo "> antlr built successfully!"
+# linix antlr4 puts output in grammar file for no reason -> moving to make consistent between os's (https://github.com/tunnelvisionlabs/antlr4ts/issues/303)
+mkdir -p mchy/built/grammar
+touch mchy/built/grammar/temp.temp
+mv mchy/built/grammar/* mchy/built
+rm -rf mchy/built/grammar
 
+if [ -f "mchy/built/MchyParser.py" ] && [ -f "mchy/built/MchyVisitor.py" ];
+then 
+    echo "> antlr build succeeded!";
+else
+    echo "> antlr build failed!";
+    echo ">> Listing built files"
+    ls -l mchy/built
+    first_file=$(ls mchy/built| head -n 1)
+    echo ">> Initial contents of '$first_file'"
+    head -n 15 $first_file
+    exit 1;
+fi
