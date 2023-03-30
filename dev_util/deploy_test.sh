0bf71778f1b6b7d68e26453dacac8cad48035a04
jacobbox <71558382+jacobbox@users.noreply.github.com>
2023-03-30 09:47:15 +0100

    Fixed force failure & made deployment only run for main

--- dev_util/deploy_test.sh
+++ dev_util/deploy_test.sh
@@ -10,7 +10,7 @@ rm -r releases/latest/dp/*
 echo "> run mchy"
 ./releases/latest/mchy.exe -v -o ./releases/latest/dp --log-file ./releases/latest/dp/mchy.log --overwrite-log ./.github/.mchy_aux_files/deploy_validate_prog.mchy
 
-exit 1;  # Force failure temp
+
 echo "> validating output"
 if [ -f "releases/latest/dp/Deploy Validate Prog/generated.txt" ];
 then 
