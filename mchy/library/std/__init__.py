c958fedb89238430c30dcc3f566f32899383dd1a
jacobbox <71558382+jacobbox@users.noreply.github.com>
2023-04-04 19:48:09 +0100

    Fixed version property

--- mchy/library/std/__init__.py
+++ mchy/library/std/__init__.py
@@ -1,5 +1,5 @@
 from mchy.library.std import (
     chain_entity_selector, chain_meta, chain_rotate, chain_scoreboard, cmd_bool, cmd_cmd, cmd_effect, cmd_fill,
     cmd_give, cmd_kill, cmd_particle, cmd_playsound, cmd_print, cmd_say, cmd_setblock, cmd_spread, cmd_summon,
-    cmd_tag, cmd_tp, mc_version, ns, struct_color, struct_pos
+    cmd_tag, cmd_tp, comp_version, ns, struct_color, struct_pos
 )
