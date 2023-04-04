1e659eb0183c8c93f2c7de158e1252c604272702
jacobbox <71558382+jacobbox@users.noreply.github.com>
2023-04-04 15:41:31 +0100

    silenced pycodestyle error

--- mchy/__main__.py
+++ mchy/__main__.py
@@ -4,5 +4,5 @@ from os import path as os_path
 sys.path.append(os_path.dirname(os_path.dirname(__file__)))
 
 # Call the command line program entry point
-from mchy.cmdln.main import main_by_cmdln
+from mchy.cmdln.main import main_by_cmdln  # noqa  #  pycodestyle doesn't like imports after ANY code even when sensible
 main_by_cmdln()
