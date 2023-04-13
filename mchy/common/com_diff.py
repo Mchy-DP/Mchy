9015da9a8aa5ac50ef541a9ea99217073770d027
jacobbox <71558382+jacobbox@users.noreply.github.com>
2023-04-13 22:53:40 +0100

    com diff improved

--- mchy/common/com_diff.py
+++ mchy/common/com_diff.py
@@ -1,7 +1,10 @@
 
 
-from typing import List
+from typing import List, Optional
 import difflib
+
+
+DID_YOU_MEAN = "Did you mean:"
 
 
 def how_similar(str1: str, str2: str) -> float:
@@ -16,4 +19,21 @@ def is_similar(str1: str, str2: str) -> bool:
 
 
 def did_you_mean_opt(bad_str: str, valid_options: List[str]) -> List[str]:
-    return difflib.get_close_matches(bad_str, valid_options)
+    opts = difflib.get_close_matches(bad_str, valid_options)
+    if len(opts) >= 2:
+        if how_similar(bad_str, opts[0]) >= (how_similar(bad_str, opts[1]) + 0.25):
+            # if theres a clear winner (first is 25% better than the best second place) then return just the best match
+            return [opts[0]]
+    return opts
+
+
+def did_you_mean_str(bad_str: str, valid_options: List[str]) -> Optional[str]:
+    """Give a string of the form `Did you mean: <OPTS>?` where OPTS are nicely formatted or None if no options could be found"""
+    did_u_mean = did_you_mean_opt(bad_str, valid_options)
+    if len(did_u_mean) == 0:
+        return None
+    # add `'s to all suggestions
+    did_u_mean = [f"`{suggestion}`" for suggestion in did_u_mean]
+    if len(did_u_mean) == 1:
+        return f"{DID_YOU_MEAN} {did_u_mean[-1]}?"
+    return f"{DID_YOU_MEAN} {' or '.join([', '.join(did_u_mean[:-1]), did_u_mean[-1]])}?"
