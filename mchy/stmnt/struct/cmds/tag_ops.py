5c54345a28dcebff097068848a3f796bd877f7cd
jacobbox <71558382+jacobbox@users.noreply.github.com>
2023-04-16 21:17:02 +0100

    fixed copy paste error

--- mchy/stmnt/struct/cmds/tag_ops.py
+++ mchy/stmnt/struct/cmds/tag_ops.py
@@ -32,7 +32,7 @@ class SmtTagMergeCmd(SmtCmd):
 
 
 class SmtTagRemoveCmd(SmtCmd):
-    """Only used for math addition (different statement for exec merging)"""
+    """Only used for math subtraction (different statement for exec merging)"""
 
     def __init__(self, target_var: SmtVar, value: SmtAtom) -> None:
         self.target_var: SmtVar = target_var
@@ -44,11 +44,11 @@ class SmtTagRemoveCmd(SmtCmd):
     def virtualize(self, linker: 'SmtLinker', stack_level: int) -> List[ComCmd]:
         target_vdat: SmtVarLinkage = linker.lookup_var(self.target_var)
         if not isinstance(target_vdat, SmtExecVarLinkage):
-            raise VirtualRepError(f"Attempted to perform tag-remove on a variable without a tag ({self.target_var.get_type()} + {self.value.get_type()})")
+            raise VirtualRepError(f"Attempted to perform tag-remove on a variable without a tag ({self.target_var.get_type()} - {self.value.get_type()})")
         if isinstance(self.value, SmtVar):
             source_vdat: SmtVarLinkage = linker.lookup_var(self.value)
             if not isinstance(source_vdat, SmtExecVarLinkage):
-                raise VirtualRepError(f"Attempted to tag-remove a variable without a tag ({self.target_var.get_type()} + {self.value.get_type()})")
+                raise VirtualRepError(f"Attempted to tag-remove a variable without a tag ({self.target_var.get_type()} - {self.value.get_type()})")
             return [ComCmd(f"tag {source_vdat.get_selector(stack_level)} remove {target_vdat.get_full_tag(stack_level)}")]
         else:
             raise VirtualRepError(f"Invalid tag-remove value type `{type(self.value)}`?")
